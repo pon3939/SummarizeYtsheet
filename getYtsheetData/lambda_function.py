4485b9182d3b22bc65af1166f607a4ca6ae202bb
pon3939 <ryuuos3939@gmail.com>
2023-06-16 05:28:28 +0900

    リファクタリング

--- getYtsheetData/lambda_function.py
+++ getYtsheetData/lambda_function.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from requests import get
+from requests import Response, get
 
 """
 ゆとシートからデータを取得
 """
 
 
-def lambda_handler(event: dict, context):
+def lambda_handler(event: dict, context) -> dict:
     """
 
     メイン処理
@@ -15,19 +15,21 @@ def lambda_handler(event: dict, context):
     Args:
         event dict: イベント
         context awslambdaric.lambda_context.LambdaContext: コンテキスト
+    Returns:
+        dict: ゆとシートから取得したデータ
     """
 
-    index = event["index"] - 1
-    player = event["players"]["Items"][index]
-    url = player["url"]["S"] + "&mode=json"
-    id = player["id"]["N"]
+    index: int = event["index"] - 1
+    player: dict = event["players"]["Items"][index]
+    url: str = player["url"]["S"] + "&mode=json"
+    id: int = player["id"]["N"]
 
     # ゆとシートにアクセス
-    response = get(url)
+    response: Response = get(url)
 
     # ステータスコード200以外は例外発生
     response.raise_for_status()
 
-    ytsheetJson = response.text
+    ytsheetJson: str = response.text
 
     return {"id": id, "ytsheetJson": ytsheetJson}
